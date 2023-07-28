3ae71024c6d1ec326e11d72daf0ebeb532df124d
RageNeko26 <rneko2006@gmail.com>
2023-07-28 23:37:39 +0700

    feat: Add more complex data to redis

--- composite.go
+++ composite.go
@@ -1,7 +1,28 @@
 package main
 
+import "encoding/json"
+
 // Should be json
 type Manga struct {
 	Title  string `json:"title"`
 	Author string `json:"author"`
+}
+
+func Composite() error {
+	json, err := json.Marshal(Manga{
+		Title:  "Attack on Titan",
+		Author: "Hajime",
+	})
+
+	if err != nil {
+		return err
+	}
+
+	err = client.Set("manga_5", json, 0).Err()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
