bbcab67f338488473fe26d9a3faa7e0b37842b0a
RageNeko26 <rneko2006@gmail.com>
2023-07-29 01:19:13 +0700

    test: JSON should returning struct Manga

--- retrieve_composite_test.go
+++ retrieve_composite_test.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"testing"
 )
 
 func TestRetrieveComposite(t *testing.T) {
 	result, err := RetrieveComposite()
 
+	// Case: Should not returning error
 	if err != nil {
 		t.Error(err)
 	}
 
-	fmt.Printf("Result: %v", *result)
+	// Case: Should returning struct and not error
+	var manga Manga
+	errUnmarshal := json.Unmarshal([]byte(*result), &manga)
+
+	if errUnmarshal != nil {
+		t.Error(errUnmarshal)
+	}
+
+	// Case: should returning Struct value as i expected
+	if manga.Title != "Attack on Titan" {
+		t.Error("Test is not as expected!")
+	}
+
+	if manga.Author != "Hajime" {
+		t.Error("Test is not as expected")
+	}
 }
